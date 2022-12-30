a3e83bd4dc3c151962ec81701106ff914af4ae26
nandinihirpara <119646455+nandinihirpara@users.noreply.github.com>
2022-12-30 10:36:11 +0530

    Add files via upload

---
+++ 2printf.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+void main()
+//program write for sqare and cube number.
+//mvariable is eqal to number.
+{
+    int m,square,cube;
+    printf("Enter a number :");
+    scanf("%d",&m);
+    square=m*m;
+    cube=m*m*m;
+    printf("square=%d\n cube=%d,",square,cube);
+}
